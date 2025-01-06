d67eac17f75acd623b4d8ed276c2d77f2ddef571
Jakub Danis <d4ny4bl3@gmail.com>
2025-01-06 21:06:12 +0100

    Change get_timestamp + VersionResponse Model

--- app/models.py
+++ app/models.py
@@ -1,17 +1,10 @@
 from pydantic import BaseModel
-from pydantic import field_validator
 from datetime import datetime
 
 
 class VersionResponse(BaseModel):
     api_version: str
-    timestamp: datetime
-    
-    # @field_validator("timestamp")
-    # def format_timestamp(cls, value):
-    #     if isinstance(value, datetime):
-    #         return value.strftime("%Y-%m-%d %H:%M:%S")
-    #     return value
+    timestamp: str
     
 
 class Transcription(BaseModel):
--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,4 @@ from datetime import datetime
 api_version = "1.0.0"
 
 def get_timestamp():
-    return datetime.now()
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
